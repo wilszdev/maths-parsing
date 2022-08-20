7d7e3b70b15408cd4b54d66544c302cc6ac16210
wilszdev <wilsdev@gmail.com>
2022-08-20 17:21:53 +1000

    remove cpp project files. just stick with python.

--- premake5.lua
+++
@@ -1,8 +0,0 @@
-workspace "MathsParsing"
-    architecture "x64"
-    configurations { "Debug", "Release", "Dist" }
-    startproject "maths_parser"
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-include "maths_parser"
